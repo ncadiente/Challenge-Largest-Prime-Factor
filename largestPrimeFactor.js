e0c7a02db9e136610dbd86459c2fd81839abf836
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-02 10:42:37 -1000

    passed test

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,30 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
+  var startNumber=n;
   // do your work here
 
-  return primeNumber;
+  function isPrime(number){
+    for(var i=2; i<number; i++){
+      if (number%i===0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function isDivisible(n, number){
+    if (n%number===0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+for (var i=(startNumber-1); i>0; i--){
+  if (isPrime(i) && isDivisible(startNumber, i)){
+    primeNumber = i;
+    return primeNumber;
+  }
+}
+
 };
